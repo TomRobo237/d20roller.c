5a8f2e353cec9d0f8c6873a6624f27c57d5c432e
Thomas Miller <thomas@TomLaptop.home>
2018-01-13 16:46:24 -0500

    Added more flags

--- d20dieroller.c
+++ d20dieroller.c
@@ -12,24 +12,26 @@
 
 int dieroller(int sides);
 int multidieroller(int dice, int sides);
-// int highroll(int dice, int sides);
-// int ac_calc(int armor, int sheild, int dex, int size_diff); May work on later
+int highroll(int dice, int sides);
 int numbercheck(char * check);
+void helpmenu();
 
 int main(int argc, char **argv) {
 	int sides = 20, dice = 1, roll, c, option_index = 0; 
+	int high_flag = 0, plus = 0, add_total;
 	/*default sides to 20 unless specified through -s or --sides flag
 	 * c is used to process getopt_long	 */
 	while  (1){
 		static struct option long_options[] =
 			{
-				{"sides", required_argument, 0, 's'},
-				//add plus number to rolls -p or --plus
+				{"side", required_argument, 0, 's'},
+				{"plus", required_argument, 0, 'p'},
 				{"dice", required_argument, 0, 'd'},
-				//highest roll?
+				{"high", no_argument, 0, 'h'},
+				{"help", no_argument,0, '?'},
 				{0,0,0,0}
 			};
-		c = getopt_long( argc, argv, "s:d:", long_options, &option_index);
+		c = getopt_long( argc, argv, "s:d:p:h?", long_options, &option_index);
 		if ( c == -1 )
 			break;
 		switch (c)
@@ -49,8 +51,7 @@ int main(int argc, char **argv) {
 				case 'd':
 					if (numbercheck(optarg) == 1)
 						{
-							fprintf( stderr , "%s is not a number of dice we can\
-							roll!\n", optarg);
+							fprintf( stderr , "%s is not a number of dice we can roll!\n", optarg);
 							exit(EXIT_FAILURE);
 						}
 					else
@@ -59,10 +60,33 @@ int main(int argc, char **argv) {
 							printf("DEBUG: Setting dice to %d\n", dice);
 							break;
 						}
-			}
+				case 'p':
+					if (numbercheck(optarg) == 1)
+						{
+							fprintf(stderr , "%s is not a number we can add!", optarg);
+							exit(EXIT_FAILURE);
+						}
+					else
+						{
+							plus = atoi(optarg);
+							printf("DEBUG: Setting Plus to %d\n", plus);
+							break;
+						}
+				case 'h':
+						high_flag = 1;
+						printf("DEBUG: Setting high_flag...\n");
+						break;
+				case '?':
+						helpmenu();
+						break;
+					}
 		}				
 	srand(time(NULL));
-	if ( dice > 1 )
+	if ( dice > 1 && high_flag == 1 )
+		{
+			roll = highroll( dice, sides );
+		}
+	else if ( dice > 1 )
 		{
 			roll = multidieroller( dice, sides );
 		}
@@ -71,6 +95,10 @@ int main(int argc, char **argv) {
 			roll = (dieroller(sides));
 			printf("Rolled d%d: %d\n", sides, roll);
 		};
+	if ( plus > 0 ) {
+		add_total = roll + plus;
+		printf("Rolled a %dd%d+%d:%d.\nTotal: %d\n", dice, sides, plus, roll, add_total);
+	} 
 	return 0;
 }
 
@@ -87,10 +115,25 @@ int multidieroller(int dice, int sides) {
 			printf("Rolling die %d:%d\n", i , 
 			rolled = ((rand() % sides) + 1 ));
 			totalrolled = rolled + totalrolled; 
-			printf("DEBUG: Total roll:%d\n", totalrolled);
 		}
 		printf("Total roll:%d\n", totalrolled);
 		return totalrolled;
+}
+
+int highroll( int dice, int sides){
+	int rolled, highestroll = 0, i;
+	printf("Rolling %dd%d and giving higest roll:\n", dice, sides);
+	for ( i = 1 ; i <= dice ; i++)
+		{
+			printf("Rolling die %d:%d\n", i , 
+			rolled = ((rand() % sides) + 1 ));
+			if ( rolled > highestroll ) 
+				{
+					highestroll = rolled; 
+				}
+		}
+		printf("Highest Roll: %d\n", highestroll);
+		return highestroll;
 }
 
 int numbercheck(char * check) {
@@ -103,4 +146,15 @@ int numbercheck(char * check) {
 		++x;
 	}
 	return 0;
+}
+
+void helpmenu() {
+	printf("Here is the help menu:\n\
+USAGE: d20dieroller [OPTIONS]...\n\n\
+Possible options:\n\t\
+--side or -s: number of sides of the dice\n\t\
+--dice or -d: number of dice to roll\n\t\
+--high or -h: takes the highest die rolled\n\t\
+--help or -?: prints this help menu\n");
+	exit(EXIT_SUCCESS);
 }
